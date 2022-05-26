ddaf1710b285237f5de6a15d5be8833b9865d9d4
Jem <jperalta@pcso.gov.ph>
2022-05-26 13:30:15 +0800

    android

---
+++ pcso-jem-mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace pcso_jem_mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ pcso-jem-mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace pcso_jem_mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
